aae1dfa22ed1edeed55f88a40dfdf3547e447ca3
aceofmath <aceofmath@naver.com>
2025-07-09 16:57:48 +0900

    초기 커밋

---
+++ src/components/landing/HighlightsWidget.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="highlights" class="py-6 px-6 lg:px-20 mx-0 my-12 lg:mx-20">
+        <div class="text-center">
+            <div class="text-surface-900 dark:text-surface-0 font-normal mb-2 text-4xl">Contact</div>
+            <!-- <span class="text-muted-color text-2xl">Amet consectetur adipiscing elit...</span> -->
+        </div>
+
+        <div class="grid grid-cols-12 gap-4 mt-20 pb-2 md:pb-20">
+            <div class="flex justify-center col-span-12 lg:col-span-8 bg-purple-100 p-0 order-1 lg:order-none" style="border-radius: 8px">
+                <!-- <img src="/demo/images/landing/mockup.svg" class="w-11/12" alt="mockup mobile" /> -->
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3165.4151768031707!2d127.02961537640346!3d37.4981249280471!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca156feedbb99%3A0x42b5932b9a140068!2z7ISc7Jq47Yq567OE7IucIOqwleuCqOq1rCDthYztl6TrnoDroZw46ri4IDExLTg!5e0!3m2!1sko!2skr!4v1725752615530!5m2!1sko!2skr"
+                    width="100%"
+                    height="450"
+                    style="border: 0"
+                    allowfullscreen=""
+                    loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </div>
+
+            <div class="col-span-12 lg:col-span-4 my-auto flex flex-col lg:items-end text-center lg:text-right gap-4">
+                <div class="flex items-center justify-center bg-purple-200 self-center lg:self-end" style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
+                    <i class="pi pi-fw pi-mobile !text-4xl text-purple-700"></i>
+                </div>
+                <div class="leading-none text-surface-900 dark:text-surface-0 text-3xl font-normal">(주)에이치쓰리연구소</div>
+                <span class="text-surface-700 dark:text-surface-100 text-2xl leading-normal ml-0 md:ml-2" style="max-width: 650px">
+                    (우)06234 서울시 강남구 테헤란로 8길 11-8, 5F
+                    <br />
+                    지하철 2호선 1번 출구에서 도보 5분</span
+                >
+            </div>
+        </div>
+
+        <!-- <div class="grid grid-cols-12 gap-4 my-20 pt-2 md:pt-20">
+            <div class="col-span-12 lg:col-span-6 my-auto flex flex-col text-center lg:text-left lg:items-start gap-4">
+                <div class="flex items-center justify-center bg-yellow-200 self-center lg:self-start" style="width: 4.2rem; height: 4.2rem; border-radius: 10px">
+                    <i class="pi pi-fw pi-desktop !text-3xl text-yellow-700"></i>
+                </div>
+                <div class="leading-none text-surface-900 dark:text-surface-0 text-3xl font-normal">Celerisque Eu Ultrices</div>
+                <span class="text-surface-700 dark:text-surface-100 text-2xl leading-normal mr-0 md:mr-2" style="max-width: 650px"
+                    >Adipiscing commodo elit at imperdiet dui. Viverra nibh cras pulvinar mattis nunc sed blandit libero. Suspendisse in est ante in. Mauris pharetra et ultrices neque ornare aenean euismod elementum nisi.</span
+                >
+            </div>
+
+            <div class="flex justify-end order-1 sm:order-2 col-span-12 lg:col-span-6 bg-yellow-100 p-0" style="border-radius: 8px">
+                <img src="/demo/images/landing/mockup-desktop.svg" class="w-11/12" alt="mockup" />
+            </div>
+        </div> -->
+    </div>
+</template>
---
+++ src/components/landing/PricingWidget.vue
@@ -0,0 +1,99 @@
+<script setup>
+// import { ref } from 'vue';
+const ports = [
+    {
+        seq: 5,
+        title: '2025 ~ 2026',
+        img: '2025-01.png',
+        period: '2025.03 ~ 2026.02',
+        name: '현대백화점 NEW 커머스 플랫폼 프로젝트 참여',
+        role: '정산, 배치',
+        ing: '진행중'
+    },
+    {
+        seq: 4,
+        title: '2023 ~ 2025',
+        img: '2023-01.png',
+        period: '2023.06 ~ 2025.03',
+        name: 'NH농협 디지털금융 플랫폼 전환 프로젝트 참여',
+        role: '금융상품몰(앱,웹,모바일웹)',
+        ing: '완료'
+    },
+    {
+        seq: 3,
+        title: '2023',
+        img: '2022-01.png',
+        period: '2023.01 ~ 2023.12',
+        name: 'SBI저축은행 수신 통합·여신 비대면 채널 구축 프로젝트 참여',
+        role: '채널(앱,웹,모바일웹)',
+        ing: '완료'
+    },
+    {
+        seq: 2,
+        title: '2022',
+        img: '2022-02.jpg',
+        period: '2022.07 ~ 2023.02',
+        name: 'NH농협 마이데이터 고도화 프로젝트 참여',
+        role: '관리자(웹), 배치',
+        ing: '완료'
+    },
+    {
+        seq: 1,
+        title: '2021',
+        img: '2021-01.jpg',
+        period: '2021.01 ~ 2021.12',
+        name: 'KB국민은행 리브 Reboot구축 프로젝트 참여',
+        role: '채널(백엔드)',
+        ing: '완료'
+    }
+];
+
+const baseUrl = '/demo/images/landing/';
+
+const productImage = (img) => {
+    return '/demo/images/landing/' + img;
+};
+</script>
+<template>
+    <div id="pricing" class="py-6 px-6 lg:px-20 my-2 md:my-6">
+        <div class="text-center mb-6">
+            <div class="text-surface-900 dark:text-surface-0 font-normal mb-2 text-4xl">Portfolio</div>
+            <!-- <span class="text-muted-color text-2xl">Amet consectetur adipiscing elit...</span> -->
+        </div>
+
+        <div class="grid grid-cols-12 gap-4 justify-between mt-20 md:mt-0">
+            <div v-for="port in ports" :key="port.seq" class="col-span-12 lg:col-span-4 p-0 md:p-4 mt-6 md:mt-0">
+                <div class="p-4 flex flex-col border-surface-200 dark:border-surface-600 pricing-card cursor-pointer border-2 hover:border-primary duration-300 transition-all" style="border-radius: 10px">
+                    <div class="text-surface-900 dark:text-surface-0 text-center my-8 text-3xl">{{ port.title }}</div>
+                    <img :src="baseUrl + port.img" class="w-10/12 mx-auto" alt="enterprise" />
+                    <!-- <div class="my-8 flex flex-col items-center gap-4">
+                        <div class="flex items-center">
+                            <span class="text-5xl font-bold mr-2 text-surface-900 dark:text-surface-0">$5</span>
+                            <span class="text-surface-600 dark:text-surface-200">per month</span>
+                        </div>
+                        <Button label="Get Started" class="p-button-rounded border-0 ml-4 font-light leading-tight bg-blue-500 text-white"></Button>
+                    </div> -->
+                    <Divider class="w-full bg-surface-200"></Divider>
+                    <ul class="my-8 list-none p-0 flex text-surface-900 dark:text-surface-0 flex-col px-8">
+                        <li class="py-2">
+                            <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                            <span class="text-xl leading-normal">{{ port.period }}</span>
+                        </li>
+                        <li class="py-2">
+                            <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                            <span class="text-xl leading-normal">{{ port.name }}</span>
+                        </li>
+                        <li class="py-2">
+                            <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                            <span class="text-xl leading-normal">{{ port.role }}</span>
+                        </li>
+                        <li class="py-2">
+                            <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                            <span class="text-xl leading-normal">{{ port.ing }}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
